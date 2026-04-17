6f4b4b009a78d853301c7cdce74b94440d2d2555
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-04-17 13:07:54 -0600

    Add files via upload

---
+++ CalculadoraBasica-25-LuisPriego/src/main/java/com/mycompany/calculadorabasica/luispriego/CalculadoraBasica25LuisPriego.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorabasica.luispriego;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class CalculadoraBasica25LuisPriego {
+
+    public static void main(String[] args) {
+
+        jfrmCalc pos = new jfrmCalc(); 
+        pos.setVisible(true);
+    }
+}
